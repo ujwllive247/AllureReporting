ebb161d4a21748fa475c146f69296d978b23e4fd
Ujjwal <ujjcgl16@gmail.com>
2025-04-03 12:23:54 +0530

    updated

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -16,8 +16,8 @@ def test_subtraction():
     assert 6 - 2 == 4
 
 
-@allure.feature("Math Operations")
-@allure.story("Multiplication")
-def test_multiplication():
-    allure.attach("Checking multiplication", name="Test Step", attachment_type=allure.attachment_type.TEXT)
-    assert 6 * 2 == 12  # Fixed wrong assertion
+# @allure.feature("Math Operations")
+# @allure.story("Multiplication")
+# def test_multiplication():
+#     allure.attach("Checking multiplication", name="Test Step", attachment_type=allure.attachment_type.TEXT)
+#     assert 6 * 2 == 12  # Fixed wrong assertion
